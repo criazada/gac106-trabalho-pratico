b92c0bbaa7972508bd9cf19cec200b72ab2ef386
matheusGonks <90430958+matheusGonks@users.noreply.github.com>
2022-03-21 20:22:28 -0300

    Esvaziando método proximaLocalizacao( )
    Também deixa de importar e instanciar a classe Random

--- simulacao/Localizacao.java
+++ simulacao/Localizacao.java
@@ -1,7 +1,5 @@
 package simulacao;
 
-import java.util.Random;
-
 /**
  * Representa uma localização no mapa
  * 
@@ -10,8 +8,6 @@ import java.util.Random;
 public class Localizacao {
     private int x;
     private int y;
-    private static Random rand = new Random();
-
     /**
      * Representa uma localização na cidade
      * 
@@ -37,29 +33,8 @@ public class Localizacao {
      * @param localizacaoDestino: localizacao que se deseja alcancar.
      * @return Localizacao para onde se deve ir
      */
-    public Localizacao proximaLocalizacao(Localizacao localizacaoDestino) {
-        if (localizacaoDestino.equals(this)) {// Verifica se já alcancou o destino
-            return localizacaoDestino;
-        } else {
-            int destX = localizacaoDestino.getX();
-            int destY = localizacaoDestino.getY();
-            int deslocX = x < destX ? 1 : x > destX ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em x
-            int deslocY = y < destY ? 1 : y > destY ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em y
-            Localizacao novaLocalizacao;
-            if (deslocX != 0 && deslocY != 0) {// Se nenhuma coordenada coincide com a localizacao destino
-                if (rand.nextInt(2) == 0) {// Atualizar x
-                    novaLocalizacao = new Localizacao(x + deslocX, y);
-                } else {// Atualizar y
-                    novaLocalizacao = new Localizacao(x, y + deslocY);
-                }
-            } else {
-                if (deslocX != 0)
-                    novaLocalizacao = new Localizacao(x + deslocX, y);
-                else
-                    novaLocalizacao = new Localizacao(x, y + deslocY);
-            }
-            return novaLocalizacao;
-        }
+    public Localizacao proximaLocalizacao(Localizacao localizacaoDestino) { 
+        return null; //deixei o metodo por dúvida se precisaremos dele depois
     }
 
     /**
