5a9706f478b4ab70fde2af39659f63f3eeae292e
iagoq <18238046+iagocq@users.noreply.github.com>
2022-04-02 16:00:13 -0300

    Remove lógica de movimentação de Localizacao (#25)

--- simulacao/Localizacao.java
+++ simulacao/Localizacao.java
@@ -1,7 +1,5 @@
 package simulacao;
 
-import java.util.Random;
-
 /**
  * Representa uma localização no mapa
  *
@@ -10,7 +8,6 @@ import java.util.Random;
 public class Localizacao {
     private int x;
     private int y;
-    private static Random rand = new Random();
 
     /**
      * Representa uma localização na cidade
@@ -29,37 +26,6 @@ public class Localizacao {
 
     public int getY() {
         return y;
-    }
-
-    /**
-     * Gera a localizacao para se mover visando alcançar o destino
-     *
-     * @param localizacaoDestino: localizacao que se deseja alcancar.
-     * @return Localizacao para onde se deve ir
-     */
-    public Localizacao proximaLocalizacao(Localizacao localizacaoDestino) {
-        if (localizacaoDestino.equals(this)) {// Verifica se já alcancou o destino
-            return localizacaoDestino;
-        } else {
-            int destX = localizacaoDestino.getX();
-            int destY = localizacaoDestino.getY();
-            int deslocX = x < destX ? 1 : x > destX ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em x
-            int deslocY = y < destY ? 1 : y > destY ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em y
-            Localizacao novaLocalizacao;
-            if (deslocX != 0 && deslocY != 0) {// Se nenhuma coordenada coincide com a localizacao destino
-                if (rand.nextInt(2) == 0) {// Atualizar x
-                    novaLocalizacao = new Localizacao(x + deslocX, y);
-                } else {// Atualizar y
-                    novaLocalizacao = new Localizacao(x, y + deslocY);
-                }
-            } else {
-                if (deslocX != 0)
-                    novaLocalizacao = new Localizacao(x + deslocX, y);
-                else
-                    novaLocalizacao = new Localizacao(x, y + deslocY);
-            }
-            return novaLocalizacao;
-        }
     }
 
     /**
--- simulacao/Veiculo.java
+++ simulacao/Veiculo.java
@@ -12,11 +12,46 @@ public class Veiculo extends ObjetoAmbulante {
         super(Recurso.CARRO.getImagem(), localizacao, mapa, rng);
     }
 
+    /**
+     * Gera a localizacao para se mover visando alcançar o destino
+     *
+     * @param localizacaoDestino: localizacao que se deseja alcancar.
+     * @return Localizacao para onde se deve ir
+     */
+    public Localizacao proximaLocalizacao() {
+        Localizacao locDestino = getLocalizacaoDestino();
+        Localizacao locAtual = getLocalizacao();
+        if (locDestino.equals(locAtual)) {// Verifica se já alcancou o destino
+            return locDestino;
+        } else {
+            int x = locAtual.getX();
+            int y = locAtual.getY();
+            int destX = locDestino.getX();
+            int destY = locDestino.getY();
+            int deslocX = x < destX ? 1 : x > destX ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em x
+            int deslocY = y < destY ? 1 : y > destY ? -1 : 0;// Deslocamento de 1 ou 0 ou -1 posição em y
+            Localizacao novaLocalizacao;
+            if (deslocX != 0 && deslocY != 0) {// Se nenhuma coordenada coincide com a localizacao destino
+                if (getRng().nextInt(2) == 0) {// Atualizar x
+                    novaLocalizacao = new Localizacao(x + deslocX, y);
+                } else {// Atualizar y
+                    novaLocalizacao = new Localizacao(x, y + deslocY);
+                }
+            } else {
+                if (deslocX != 0)
+                    novaLocalizacao = new Localizacao(x + deslocX, y);
+                else
+                    novaLocalizacao = new Localizacao(x, y + deslocY);
+            }
+            return novaLocalizacao;
+        }
+    }
+
     @Override
     public void executarAcao() {
         Localizacao destino = getLocalizacaoDestino();
         if (destino != null) {
-            Localizacao prox = getLocalizacao().proximaLocalizacao(destino);
+            Localizacao prox = proximaLocalizacao();
             // carro só anda se o espaço de destino está livre
             if (livre(prox)) {
                 setLocalizacao(prox);
