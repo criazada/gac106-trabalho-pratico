21b38d078c222ef40311c4970edb5e7578b0462c
iagocq <18238046+iagocq@users.noreply.github.com>
2022-04-02 16:45:42 -0300

    Adiciona ícones variados

--- simulacao/Recurso.java
+++ simulacao/Recurso.java
@@ -4,10 +4,13 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public enum Recurso {
-    CARRO("carro.jpg");
+    CARRO("carro.png"),
+    SEMAFORO_VERDE("semaforo-verde.png"),
+    SEMAFORO_AMARELO("semaforo-amarelo.png"),
+    SEMAFORO_VERMELHO("semaforo-vermelho.png");
 
     private Image imagem;
-    private static final String base = "Imagens";
+    private static final String base = "imagens";
 
     private Recurso(String nome) {
         imagem = new ImageIcon(getClass().getResource(base + "/" + nome)).getImage();
--- simulacao/Semaforo.java
+++ simulacao/Semaforo.java
@@ -4,18 +4,33 @@ import java.util.Random;
 
 public class Semaforo extends ObjetoSimulacao {
     private int contador;
+    private boolean transparente;
 
     public Semaforo(Localizacao localizacao, Mapa mapa, Random rng) {
-        super(Recurso.CARRO.getImagem(), localizacao, mapa, Mapa.Camada.MIDDLE, rng);
+        super(Recurso.SEMAFORO_VERMELHO.getImagem(), localizacao, mapa, Mapa.Camada.MIDDLE, rng);
     }
 
     @Override
     public void executarAcao() {
         contador++;
+        
+        transparente = true;
+        int t = contador % 20;
+        Recurso r;
+        if (t < 10) {
+            r = Recurso.SEMAFORO_VERDE;
+        } else if (t < 13) {
+            r = Recurso.SEMAFORO_AMARELO;
+        } else {
+            r = Recurso.SEMAFORO_VERMELHO;
+            transparente = false;
+        }
+
+        setImagem(r.getImagem());
     }
 
     @Override
     public boolean transparentePara(ObjetoSimulacao o) {
-        return contador % 10 < 5;
+        return transparente;
     }
 }
